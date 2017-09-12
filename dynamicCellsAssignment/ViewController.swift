9306872151a9d8622253a8d1492f4d1ae52678f6
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2017-09-11 21:15:44 -0700

    iOS Swift Dynamic Cells Assignment

--- dynamicCellsAssignment/ViewController.swift
+++ dynamicCellsAssignment/ViewController.swift
@@ -8,10 +8,29 @@
 
 import UIKit
 
+var tasks: [String] = ["-----To-Do List 3000-----"]
+
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if let message = self.taskTextField.text{
+            if message.characters.count > 0{
+                tasks.append(message)
+            }
+        }
+        self.taskTextField.text = "" //empty text field after enter
+        tableView.reloadData()
+        print (taskTextField.text!)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,4 +41,36 @@ class ViewController: UIViewController {
 
 
 }
+
+//What data do i want to display?
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
+//Delete tasks/cell
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
+}
+
+
+
+
+
+
+
+
 
